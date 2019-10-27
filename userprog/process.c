07200f6dc34f66c0d5c7b74f58de4149df62c540
Jeffxzj <xuzj@shanghaitech.edu.cn>
2019-10-27 22:18:44 +0800

    weely push

--- userprog/process.c
+++ userprog/process.c
@@ -65,6 +65,7 @@ start_process (void *file_name_)
   palloc_free_page (file_name);
   if (!success) 
     thread_exit ();
+  
 
   /* Start the user process by simulating a return from an
      interrupt, implemented by intr_exit (in
