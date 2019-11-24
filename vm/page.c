fe4e5689a4a8e06542f96f482b9b008b7f0dc02e
Runze Yuan <runze.yuan@stereye.com>
2019-11-25 00:40:55 +0800

    spte

--- vm/page.c
+++ vm/page.c
@@ -7,7 +7,7 @@ page_hash_func (const struct hash_elem *p, void *aux UNUSED)
     const struct page_suppl_entry * e = hash_entry (p, struct page_suppl_entry,
                                                         elem);
 
-    return has_bytes (&e->upage, sizeof (e->upage));
+    return hash_bytes (&e->upage, sizeof (e->upage));
 }
 
 bool
@@ -23,21 +23,24 @@ page_hash_less_func (const struct hash_elem *a,
     return e1->upage < e2->upage;
 }
 
+
 struct page_suppl_entry *
-page_create_spte ( struct file *file, off_t offset, uint8_t *upage,
+page_create_spte ( struct file *file, off_t offset, uint8_t *upage, int type,
                     uint32_t read_bytes, uint32_t zero_bytes, bool writable)
 {
     struct page_suppl_entry *e;
     e = malloc(sizeof(struct page_suppl_entry));
     if (e == NULL)
         return NULL;
-        
+
+    e->type = type;
     e->file = file;
     e->ofs = offset;
     e->upage = upage;
     e->read_bytes = read_bytes;
     e->zero_bytes = zero_bytes;
     e->writable = writable;
+    e->loaded = false;
     
     return e;
 }
@@ -54,4 +57,39 @@ page_hash_insert (struct hash *table, struct page_suppl_entry *e)
     }
 
     return false;
+}
+
+struct page_suppl_entry *
+page_hash_find (struct hash *table, uint8_t *upage)
+{
+    if (upage == NULL || table == NULL)
+        return false;
+    /* Generate a pseudo entry using for search */
+    upage = pg_round_down (upage);
+    struct page_suppl_entry search;
+    search.upage = upage;
+
+    /* Search the corresponding page */
+    struct hash_elem *matched_elem = hash_find (table, &search.elem);
+    if (matched_elem == NULL)
+        return NULL;
+
+    /* Transform elem to entry */
+    struct page_suppl_entry *matched_page;
+    matched_page = hash_entry (matched_elem, struct page_suppl_entry,elem);
+
+    return matched_page;
+}
+
+bool page_load (struct page_suppl_entry *e)
+{
+    switch (e->type) 
+    {
+        case MMAP:
+            page_load_file (e);
+        case SWAP:
+            page_load_swap (e);
+        case FILE:
+            page_load_file (e);
+    }
 }
--- vm/page.h
+++ vm/page.h
@@ -1,6 +1,9 @@
 #include "filesys/file.h"
 #include "lib/kernel/hash.h"
 
+#define FILE 1
+#define SWAP 2
+#define MMAP 3
 
 struct page_suppl_entry {
     struct file* file;
@@ -10,11 +13,12 @@ struct page_suppl_entry {
     uint32_t read_bytes; 
     uint32_t zero_bytes; 
     bool writable;
+    bool loaded;
     struct hash_elem elem;  
 };
 
 struct page_suppl_entry*
-page_create_spte (struct file *file,  off_t offset, uint8_t *upage,
+page_create_spte (struct file *file,  off_t offset, uint8_t *upage, int type,
                     uint32_t read_bytes, uint32_t zero_bytes, bool writable);
 
 
@@ -29,3 +33,12 @@ page_hash_less_func (const struct hash_elem *a_, const struct hash_elem *b_,
 
 bool
 page_hash_insert (struct hash *table, struct page_suppl_entry *e);
+
+struct page_suppl_entry *
+page_hash_find (struct hash *table, uint8_t *upage);
+
+bool page_load_file (struct page_suppl_entry *e);
+bool page_load_swap (struct page_suppl_entry *e);
+bool page_load_mmp (struct page_suppl_entry *e);
+
+bool page_load (struct page_suppl_entry *e);
