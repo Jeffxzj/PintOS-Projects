514f68534a88b3f4f1faf5757e23cf806694194a
Runze Yuan <runze.yuan@stereye.com>
2019-11-25 23:59:21 +0800

    page_load_file

--- vm/page.h
+++ vm/page.h
@@ -3,26 +3,32 @@
 
 #include <hash.h>
 #include "filesys/file.h"
+#include "frame.h"
 
-#define FILE 1
-#define SWAP 2
-#define MMAP 3
+enum spte_type 
+{
+  FILE,
+  SWAP,
+  MMAP
+};
 
-struct page_suppl_entry {
-    struct file* file;
-    int type;
-    off_t ofs; 
-    uint8_t *upage;
-    uint32_t read_bytes; 
-    uint32_t zero_bytes; 
-    bool writable;
-    bool loaded;
-    struct hash_elem elem;  
+struct page_suppl_entry 
+{
+  struct file* file;
+  enum spte_type type;
+  off_t ofs; 
+  uint8_t *upage;
+  uint32_t read_bytes; 
+  uint32_t zero_bytes; 
+  bool writable;
+  bool loaded;
+  struct hash_elem elem;  
 };
 
 struct page_suppl_entry*
-page_create_spte (struct file *file,  off_t offset, uint8_t *upage, int type,
-                    uint32_t read_bytes, uint32_t zero_bytes, bool writable);
+page_create_spte (struct file *file,  off_t offset, uint8_t *upage, 
+                  enum spte_type type, uint32_t read_bytes,
+                  uint32_t zero_bytes, bool writable);
 
 
 /* Returns a hash value for page p. */
@@ -31,8 +37,8 @@ page_hash_func (const struct hash_elem *p_, void *aux);
 
 /* Returns true if page a precedes page b. */
 bool
-page_hash_less_func (const struct hash_elem *a_, 
-                     const struct hash_elem *b_,
+page_hash_less_func (const struct hash_elem *a, 
+                     const struct hash_elem *b,
                      void *aux);
 
 bool
